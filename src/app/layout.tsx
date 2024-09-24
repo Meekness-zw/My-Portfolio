1c2c63f68ea3810931864177e0c3d65c105f543c
Meekness Kaboti <mikmarkson@gmail.com>
2024-09-24 14:57:08 +0200

    My Portfolio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import TransitionProvider from "./components/transitionProvider"
+import TransitionProvider from "./components/TransitionProvider"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
